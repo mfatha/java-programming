85544f5c9d12458540bcbc3a832d3f293ccc2b7e
Mohammed Fathauddin <mfatha.fire@gmail.com>
2018-03-28 00:00:41 +0530

    log4j2  issues and assembler code fix...

--- assembler-utility/run-utility.bat
+++ assembler-utility/run-utility.bat
@@ -1,1 +1,1 @@
-java -Djava.library.path=./lib -Xmx4G -Dexterro.zeta.log.level=DEBUG -jar ./lib/websurf-utility-0.1.jar
+java -Dlog4j.configurationFile=file:log4j2.xml -Djava.library.path=./lib -Xmx4G -Dexterro.zeta.log.level=DEBUG -jar ./lib/websurf-utility-0.1.jar
