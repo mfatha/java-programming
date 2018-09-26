0e38e29fbb53a17222543cdb526362baf34f1bfb
Mohammed Fathauddin <mfatha.fire@gmail.com>
2018-09-26 16:04:39 +0530

    test-instagram utility

---
+++ utilities/instagram-utility/src/com/munna/instagram/drivers/InstagramDriver.java
@@ -0,0 +1,16 @@
+package com.munna.instagram.drivers;
+
+
+/**
+ * @author Mohammed Fathauddin
+ * @since 2018
+ * @credit https://github.com/brunocvcunha/instagram4j
+ */
+public class InstagramDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
